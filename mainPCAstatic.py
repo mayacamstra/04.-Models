e1519d11c6b7216ad090622c80ab2e8e886b6719
mayac <maya.camstra@ortec-finance.com>
2024-08-29 21:56:43 +0200

    't+45' gelukt

--- mainPCAstatic.py
+++ mainPCAstatic.py
@@ -1219,6 +1219,31 @@ for num_factors in factor_range:
     predicted_variables_dict[num_factors] = np.hstack((predicted_variables_dict[num_factors], predicted_variables_t44.T))
     # print(f"Predicted variables for {next_timestamp_44_str}:\n", predicted_variables_t44)
 
+	# Voeg de voorspelde waarden voor 't+44' toe aan de trainingsdata
+    extended_train_data_44 = np.hstack((extended_train_data, predicted_variables_t44.T))
+    extended_train_data_44_std = standardize(extended_train_data_44.T).T
+    extended_index_44 = extended_index + [next_timestamp_44]
+    extended_train_df_44 = pd.DataFrame(extended_train_data_44_std, index=Y_train.index, columns=extended_index_44)
+    model = DynamicFactorModel(extended_train_df_44, num_factors)
+    model.std_data = extended_train_data_44_std.T
+    model.apply_pca()
+    model.yw_estimation()
+    fac_train_extended_44 = model.factors.T
+    data_train_extended_44 = extended_train_data_44_std.T
+    print("Training extended model for t+45 with data and factors...")
+    model.enet_fit(data_train_extended_44, fac_train_extended_44)
+    if model.model_ena is None:
+        raise ValueError("ElasticNet model is not set after fitting. Check enet_fit method.")
+    next_timestamp_45 = next_timestamp_44 + 1
+    next_timestamp_45_str = next_timestamp_45.strftime('%Y-%m')
+    factor_forecast_45 = model.factor_forecast(next_timestamp_45_str, scenarios=1)
+    if factor_forecast_45.shape[1] != num_factors:
+        raise ValueError(f"Expected {num_factors} features, got {factor_forecast_45.shape[1]} features")
+    predicted_factors_dict[num_factors] = np.hstack((predicted_factors_dict[num_factors], factor_forecast_45.T))
+    predicted_variables_t45 = model.enet_predict(factor_forecast_45.reshape(1, -1))
+    predicted_variables_dict[num_factors] = np.hstack((predicted_variables_dict[num_factors], predicted_variables_t45.T))
+    # print(f"Predicted variables for {next_timestamp_45_str}:\n", predicted_variables_t45)
+
     # Calculate RMSE and R² for in-sample and test data
     rmse_value_in_sample = RMSE(data_train, y_hat_train)
     rmse_value_test_sample = RMSE(data_test, y_hat_test)
